4aa1c00c6662f2e0b5c6fc2dffe414062c008e1f
root <root@Sarman>
2025-03-12 16:34:34 +0530

    cors

--- server.js
+++ server.js
@@ -20,7 +20,12 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin:'https://blockza-events-frontend.netlify.app',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
